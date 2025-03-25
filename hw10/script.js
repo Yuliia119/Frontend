18e2d9a11a170f165ab07ac65ea33b84ec46d00c
Yuliia <uliagrinenko1981@gmail.com>
2025-03-25 13:19:43 +0100

    add

---
+++ hw10/script.js
@@ -0,0 +1,11 @@
+//  Задание 1
+let n1 = 100;
+let n2 = 25 % 3;
+let myNumber = 100 / n2;
+console.log(myNumber);
+document.getElementById('task1').innerHTML = `1. Результат: ${myNumber}`;
+// Задание 2
+let myString = "Hello " + " world" + "!" +` 2024`;
+console.log(myString);
+document.getElementById('task2').innerHTML = `2. Результат: ${myString}`;
+
