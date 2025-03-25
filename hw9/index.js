10966a7bea5f6daf106b41ceb91399d3322b7f1a
Yuliia <uliagrinenko1981@gmail.com>
2025-03-25 09:58:17 +0100

    add

--- hw9/index.js
+++ hw9/index.js
@@ -4,12 +4,12 @@ const btn = document.getElementById("magic-btn");
 const clonedBtn = btn.cloneNode(true);
 // задали id
 clonedBtn.id = "magic-btn-2";
-clonedBtn.textContent = "Я изменю тебя";
-clonedBtn.style.backgroundColor = "#a78b71";
-clonedBtn.style.color = "white";
-clonedBtn.classList.add("second-btn");
 
 btn.addEventListener('click', () => {   // клик на первую кнопку
+  clonedBtn.classList.add("second-btn");
+  clonedBtn.style.color = "white";
+  clonedBtn.style.backgroundColor = "#a78b71";
+  clonedBtn.textContent = "Я изменю тебя";
   document.body.appendChild(clonedBtn);  // добавляем вторую кнопку в body
   
 });
