db00e0d6de57c358e5c9f3675dbdcc3ec6b06cc7
Yuliia <uliagrinenko1981@gmail.com>
2025-03-27 17:01:28 +0100

    corr

--- hw12/script.js
+++ hw12/script.js
@@ -6,30 +6,40 @@ const starWarsHeroes = [
     { name: "Obi-Wan Kenobi", age: 60, isJedi: true },
     ];
     
-    // Task 1
+    // Задание 1 - создаем новый массив с элнментами: name и isJedi
     const starWarsHeroesNew = (list) => {
     return list.map((el) => ({ name: el.name, isJedi: el.isJedi }));
     };
+    console.log(starWarsHeroesNew(starWarsHeroes));
+    document.getElementById('task1').innerText = "1. Результат: "+ JSON.stringify(starWarsHeroesNew(starWarsHeroes))  //выводим на экран
     
-    // console.log(starWarsHeroesNew(starWarsHeroes));
-    
-    // Task 2
+    // Задание 2 - новый массив с isJedi младше 40 лет
     const starWarsHeroesYung = starWarsHeroes.filter((el) => el.age < 40);
+    console.log(starWarsHeroesYung);
+    document.getElementById('task2').innerText = "2. Результат: "+ JSON.stringify(starWarsHeroesYung)  //выводим на экран
     
-    // console.log(starWarsHeroesYung);
-    
+    // Задание 3 - суммируем возраст всех джидаев (isJedi = true)
     function sumOfStarWarsHeroes(list) {
     return list
     .filter((el) => el.isJedi === true)
     .map((el) => el.age)
     .reduce((acc, current) => acc + current, 0);
     }
+    console.log(sumOfStarWarsHeroes(starWarsHeroes));
+    document.getElementById('task3').innerText = "3. Результат: "+ JSON.stringify(sumOfStarWarsHeroes(starWarsHeroes))  //выводим на экран
     
-    // console.log(sumOfStarWarsHeroes(starWarsHeroes));
-    
-    // Task 3
-    
+    // Задание 4 - повышаем возраст героев на 10 лет
     const starWarsHeroesGrownUp = starWarsHeroes.map((el) => el.age + 10);
-    
-    // console.log(starWarsHeroesGrownUp);
+    console.log(starWarsHeroesGrownUp);
+    document.getElementById('task4').innerText = "4. Результат: "+ JSON.stringify(starWarsHeroesGrownUp)  //выводим на экран
+
+    // Задание 5 - создаём новый массив, где "Anakin Skywalker" заменен на:`{ name: "Darth Vader", isJedi: false, age: 50 }`
+    function starWarsHeroesDarkSide(list) {
+    const result = list.slice(1);
+    result.unshift({ name: "Darth Vader", isJedi: false, age: 50 });
+    return result;
+    }
+    console.log(starWarsHeroesDarkSide(starWarsHeroes));
+    document.getElementById('task5').innerText = "5. Результат: "+ JSON.stringify(starWarsHeroesDarkSide(starWarsHeroes))  //выводим на экран
+
     
